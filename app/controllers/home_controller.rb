904375214deb9778bb6793fc7ea518fc5f2a2977
Muhammad Azahrul Ramadhan <90820918+Azahrul@users.noreply.github.com>
2024-06-04 05:14:54 +0000

    commit halaman baca

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def read
-    @article = Article.where(id: params[:article_id])
+    @article = Article.where(id: params[:article_id]).first
   end
 end
 
